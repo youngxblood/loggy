242fb9f90bfa923a6ef47e23dd28c680b5fa08fb
youngxblood <crazyeight68@gmail.com>
2017-03-02 22:43:24 -0800

    Initial commit.
    Signed-off-by: youngxblood <crazyeight68@gmail.com>

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Layout Testing</title>
+</head>
+<body>
+<header>
+    <h1>Loggy: For You</h1>
+    <br/>
+    <span class="date"> <?php echo date("l") ?> </span>
+</header>
+<div id="wrapper">
+    <h3>
+        TESTING WRAPPERS:
+    </h3>
+    <br/>
+    <p>
+        Here I am testing my wrappers to make sure they work. I am also testing my 'p' tags and their styling.
+    </p>
+    <p>
+        Here I am testing my wrappers to make sure they work. I am also testing my 'p' tags and their styling.
+    </p>
+    <p>
+        Here I am testing my wrappers to make sure they work. I am also testing my 'p' tags and their styling.
+    </p>
+    <br/>
+
+
+</div>
+
+
+</body>
+</html>
